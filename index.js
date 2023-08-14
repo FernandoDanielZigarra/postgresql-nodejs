bbfcb732d0393345c215e835fece56045d7eb71c
unknown <fernando.zigarra@gmail.com>
2023-08-14 09:52:20 -0300

    postgress added

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-import express from 'express'
+import express from "express";
+import pg from "pg";
+import dotenv from 'dotenv';
+dotenv.config()
 
-const app = express()
+const app = express();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:true
+});
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/ping", async(req, res) => {
+  const result = await pool.query('SELECT NOW()')
+  return res.json(result.rows[0]);
+});
+
+
 
 app.listen(3000, () => {
-    console.log('server in port 3000')
-})
+  console.log("server in port 3000");
+});
